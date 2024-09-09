93e2a5468bc7bb1cda10a00d30b492e226757483
Dolf <dolfbosch@icloud.com>
2024-09-09 21:14:26 +0200

    Finished all exercises!

--- week_1_exercises/4/ex4.cc
+++ week_1_exercises/4/ex4.cc
@@ -1,8 +1,7 @@
 #include <iostream>
 
 char const message[] = 
-R"R(
-^\s+Encryption key:(\w+)
+R"R(^\s+Encryption key:(\w+)
 ^\s+Quality=(\d+)
 ^\s+E?SSID:"([[:print:]]+)"
 ^\s+ssid="([[:print:]]+)"
--- week_1_exercises/7/program.cc
+++ week_1_exercises/7/program.cc
@@ -2,12 +2,27 @@
 
 using namespace std;
 
-union value
+union u 
 {
+    size_t value;
+    struct 
+    {
+        size_t bit_0: 1;
+        size_t bits_1_4: 3;
+        size_t bits_4_8: 4;
+        size_t bits_8_13: 5;
+        size_t bits_13_17: 4;
+        size_t bits_17_21: 4;
+        size_t bits_21_25: 4;
+        size_t bits_25_29: 4;
+        size_t bits_29_33: 4;
+        size_t bits_33_38: 5;
+        size_t bits_38_40: 2;
+    } bitfield;
 
-}
+};
 
 int main()
 {
-    //cout << hex
+    cout << hex << u{.bitfield{0,7,15,10,6,7,15,15,7,0,3}}.value;
 }
